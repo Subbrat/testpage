44ff2fdfd511d82b9cba3b865a00432a88071b8e
Subrat Mallick <m.subrat198470@gmail.com>
2022-06-16 19:21:27 +0530

    sas

--- js/main.js
+++ js/main.js
@@ -53,3 +53,15 @@
         })
     });
 }
+{
+    {
+        window.addEventListener('mouseup', function (event) {
+            var box = document.getElementById('mySidebar');
+            if (event.target != box && event.target.parentNode != box) {
+                box.style.display = 'none';
+            }
+        });
+    }
+
+
+}
